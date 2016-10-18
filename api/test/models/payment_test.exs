6ac9a966acc21143bfd4f9b3b9d9a0c64ba764a8
Flávio <flavio.mv@gmail.com>
2016-10-17 23:42:52 -0200

    tests Payment

---
+++ api/test/models/payment_test.exs
@@ -0,0 +1,28 @@
+defmodule Api.PaymentTest do
+  use Api.ModelCase
+
+  alias Api.Payment
+
+  @valid_attrs %{
+    amount: "120.5",
+    intermediaries: [],
+    card: %{
+      cvv: "111",
+      expiration_month: "FEB",
+      expiration_year: "2016",
+      holder: "Name",
+      number: "1111111111111111"
+    }
+  }
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Payment.changeset(%Payment{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Payment.changeset(%Payment{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
