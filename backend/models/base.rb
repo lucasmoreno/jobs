06d643daa1398adc5b1453bdc41d2cf664c3fbd1
Lucas Moreno Silva <lucasmorenosilva@gmail.com>
2017-08-10 01:31:07 -0300

    Add #valid? method to base model

--- backend/models/base.rb
+++ backend/models/base.rb
@@ -3,5 +3,30 @@ module Models
     include ActiveModel::Model
     include ActiveModel::Validations
     include Virtus.model
+    
+    # @return [Boolean]
+    def valid?(context = nil)
+      super
+      attributes.each do |key, value|
+        if value.is_a? Array
+          value.each { |element| add_errors(key: key, model: element) }
+        else
+          add_errors(key: key, model: value)
+        end
+      end
+
+      errors.empty?
+    end
+
+    private
+
+    # @param key [String]
+    # @param model [Object]
+    # @return [nil]
+    def add_errors(key:, model:)
+      if model.class.ancestors.include? ActiveModel::Validations
+        errors.messages[key] = model.errors.full_messages if model.invalid?
+      end
+    end
   end
 end
